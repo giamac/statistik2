9a6ab6b9817f60fa13e2448ca669536e47c996e2
giamac <gmacauda@gmx.net>
2018-03-19 10:05:41 +0100

    added bonferroni-holm

--- posthocs.R
+++ posthocs.R
@@ -8,7 +8,7 @@ ggplotly(t_pure)
 t_krit_uncorr <- t_pure + geom_vline(xintercept = qt((1-.05/2), df = 12-4)) + geom_vline(xintercept = -qt((1-.05/2), df = 12-4))
 ggplotly(t_krit_uncorr)
 
-# T-Werte bei .99 Quantil
+# T-Werte bei .99 Quantil SIDAK CORRECTION
 
 t_krit_adj <- t_krit_uncorr + geom_vline(xintercept = -2.8965, col = 'red') + geom_vline(xintercept = 2.8965, col = 'red')
 ggplotly(t_krit_adj)
@@ -25,6 +25,26 @@ t_emp_both <- t_krit_adj + geom_vline(xintercept = 2.4495, col = 'blue') + geom_
 
 ggplotly(t_emp_both)
 
-# Exakte t-werte
-t_krit_adj_exact <- t_krit_uncorr + geom_vline(xintercept = qt(.98735, df = 12-4), col = 'red') + geom_vline(xintercept = -qt(.98735, df = 12-4), col = 'red')
-ggplotly(t_krit_adj_exact)
+# # Exakte t-werte
+# t_krit_adj_exact <- t_krit_uncorr + geom_vline(xintercept = qt(.98735, df = 12-4), col = 'red') + geom_vline(xintercept = -qt(.98735, df = 12-4), col = 'red')
+# ggplotly(t_krit_adj_exact)
+
+# Bonferroni Holm Methode
+
+#0.05 Niveau (keine Korrektur)
+
+ggplotly(t_krit_uncorr)
+
+# Reihenfolge: Erster Vergleich
+t_krit_corr_first <- t_krit_uncorr + geom_vline(xintercept = -qt(1 - (0.025/2), df = 8), col = 'red') + geom_vline(xintercept = qt(1 - (0.025/2), df = 8), col = 'red')
+ggplotly(t_krit_corr_first)
+
+t_emp1_holm <- t_krit_corr_first + geom_vline(xintercept = -4.0825, col = 'blue')
+
+ggplotly(t_emp1_holm)
+
+t_krit_corr_second <- t_krit_uncorr + geom_vline(xintercept = -qt(1 - (0.05/2), df = 8), col = 'red') + geom_vline(xintercept = qt(1 - (0.05/2), df = 8), col = 'red')
+ggplotly(t_krit_corr_second)
+
+t_emp2_holm <- t_krit_corr_second + geom_vline(xintercept = 2.4495, col = 'blue')
+ggplotly(t_emp2_holm)
