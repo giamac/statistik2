7ac591743e54d0f3141856dc5aaa8708113c06fb
giamac <gmacauda@gmx.net>
2018-03-26 11:04:21 +0200

    plots for twofactorial design

---
+++ zweifaktoriell.R
@@ -0,0 +1,35 @@
+### Zweifaktorielle Varianzanalyse ###
+
+## Alkoholbeispiel ## 
+
+aggro <- c(69,73,74,74,79,72,71,69,67,64,58,64)
+
+alk <- c(rep('ja', each = 3), rep('nein', each = 3))
+
+erwartung <- c(rep('ja', each = 6), rep('nein', each = 6))
+
+df <- data.frame(aggro, alk, erwartung)
+
+library(dplyr)
+
+summarised_df <- df %>%
+  group_by(alk, erwartung) %>%
+  summarise(meanAggro = mean(aggro))
+
+main_effect_alk <- df %>%
+  group_by(alk) %>%
+  summarise(meanAggro = mean(aggro))
+
+library(ggplot2)
+
+effects_plot1 <- summarised_df %>%
+  ggplot(aes(x= alk, y = meanAggro, color = erwartung, group = erwartung)) +
+  geom_point(size = 4) + 
+  geom_line(size = 1) +
+  geom_jitter(data = df, alpha = 0.3, position = position_jitter(width = 0.1), size = 2, aes (x = alk, y = aggro, group = erwartung, color = erwartung))
+
+effects_plot2 <- summarised_df %>%
+  ggplot(aes(x= erwartung, y = meanAggro, color = alk, group = alk)) +
+  geom_point(size = 4) + 
+  geom_line( size = 1) +
+  geom_jitter(data = df, alpha = 0.3, position = position_jitter(width = 0.1), size = 2, aes (x = erwartung, y = aggro, group = alk, color = alk))
